c4b66f41efb25dad976915f92371cf02bba7866f
kongji <ahdlrhtkk@naver.com>
2017-01-12 02:40:25 +0900

    1. 가로메뉴 폭 조정 150px->100px 2. 리프레시 버튼 기본 off 로 설정

--- sanview/js/timer.js
+++ sanview/js/timer.js
@@ -12,7 +12,7 @@ var timer = new (function() {
 		welBtnRefresh = $(".timer .refresh");
 		welCountdown = $(".timer .countdown");
 		
-		welBtnRefresh.click(refresh);
+		welBtnRefresh.click(clickRefresh);
 	}
 	
 	this.start = function(intervalSec, _onRefresh) {
@@ -21,10 +21,11 @@ var timer = new (function() {
 		
 		clearTimeout(tCounter);
 		
-		isProgress = true;
+		isProgress = false;
+		paint();
 		
-		tCounter = setInterval(countdown, 1000);
-		countdown();
+//		tCounter = setInterval(countdown, 1000);
+//		countdown();
 	}
 	
 	var countdown = function() {
@@ -32,27 +33,36 @@ var timer = new (function() {
 			return;
 		}
 		remainSec--;
-		var msg = remainSec + "초";
-		if (remainSec % 30 > 24) {
-			msg = "refresh ON " + remainSec + "초";
-		}
-		welCountdown.text(msg);
+		paint();
 		if (remainSec <= 0) {
 			onRefresh();
 		}
 	}
 	
-	var refresh = function(we) {
+	var clickRefresh = function(we) {
 		we.preventDefault();
 		isProgress ^= true;
-		welBtnRefresh.removeClass("btn-warning btn-primary").addClass(isProgress ? "btn-warning" : "btn-primary");
 		
 		clearTimeout(tCounter);
 		if (isProgress) {
 			tCounter = setInterval(countdown, 1000);
 			countdown();
 		} else {
-			welCountdown.text("refresh OFF");
+			paint();
 		}
+	}
+	
+	var paint = function() {
+		welBtnRefresh.removeClass("btn-warning btn-primary").addClass(isProgress ? "btn-warning" : "btn-primary");
+		var msg = "";
+		if (isProgress) {
+			msg = remainSec + "초";
+			if (remainSec % 30 > 24) {
+				msg = "refresh ON " + remainSec + "초";
+			}
+		} else {
+			msg = "refresh OFF";
+		}
+		welCountdown.text(msg);
 	}
 })();
